41f2abf2a56cd5e2c5faf0d71db61c8910fba171
germanovna1 <vatakivi@yandex.ru>
2022-11-15 20:44:03 +0300

    Second

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         int amount = 2500;
-        boolean isBye = true;
 
-        int bonus;
-        if (isBye) {
-            bonus = 1;
-        } else {
-            bonus = 0;
+        int bonus = 1;
+        if (bonus > 0) {
+
         }
 
         int mile = amount / 20 * bonus;
