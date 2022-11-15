7a0af6b67fde25ccc1d31b05e56ec2d576170e67
germanovna1 <vatakivi@yandex.ru>
2022-11-15 20:48:39 +0300

    Third

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int amount = 2500;
 
-        int bonus = 1;
-        if (bonus > 0) {
+        int bonus = 1;{
 
         }
 
