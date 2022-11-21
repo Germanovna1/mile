4988a5a74f77603fed5295dbd77b12260f11b8af
germanovna1 <vatakivi@yandex.ru>
2022-11-21 20:14:35 +0300

    Deleted_parentheses

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int amount = 2500;
+        int amount = 10000;
 
-        int bonus = 1;{
-
-        }
+        int bonus = 1;
 
         int mile = amount / 20 * bonus;
         System.out.println("Итого миль:" + mile);
